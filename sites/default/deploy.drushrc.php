595cac68274ad5bee033950a681274e3d18c20dd
Goruachev42 <mikhail.goruachev@lemberg.co.uk>
2014-10-21 14:18:22 +0300

    hidr

--- sites/default/deploy.drushrc.php
+++ sites/default/deploy.drushrc.php
@@ -8,14 +8,14 @@ $options['docroot'] = '/var/www/vhosts/drushdep.uat.link/httpdocs/';
 $options['ssh-options'] = '-o PasswordAuthentication=yes';
 $options['git_enable_submodules'] = TRUE;
 // Run additional tasks after the 'current' symlink has been updated
-$options['after']['deploy-symlink'][] = 'my_custom_task';
-
-/**
- * The task needs to be defined with a @task "decorator" in the comment block preceding it
- * @task
- */
-function my_custom_task($d) {
-    $d->run("ln -s /var/www/vhosts/drushdep.uat.link/deploy/shared/settings.php %s/sites/default/settings.php", $d->latest_release());
-    $d->run("ln -s /var/www/vhosts/drushdep.uat.link/deploy/shared/files %s/sites/default/files", $d->latest_release());
-}
+//$options['after']['deploy-symlink'][] = 'my_custom_task';
+//
+///**
+// * The task needs to be defined with a @task "decorator" in the comment block preceding it
+// * @task
+// */
+//function my_custom_task($d) {
+//    $d->run("ln -s /var/www/vhosts/drushdep.uat.link/deploy/shared/settings.php %s/sites/default/settings.php", $d->latest_release());
+//    $d->run("ln -s /var/www/vhosts/drushdep.uat.link/deploy/shared/files %s/sites/default/files", $d->latest_release());
+//}
 ?>
