2b86640ea3d95bce466828c5b7e5fe1dbf042edc
Goruachev42 <mikhail.goruachev@lemberg.co.uk>
2014-10-22 12:56:43 +0300

    fff

--- sites/default/deploy.drushrc.php
+++ sites/default/deploy.drushrc.php
@@ -11,16 +11,16 @@ $options['docroot'] = '/var/www/vhosts/drushdep.uat.link/httpdocs/';
 $options['git_enable_submodules'] = TRUE;
 // Run additional tasks after the 'current' symlink has been updated
 
-$options['after']['deploy-symlink'][] = 'deploy_cache_task';
+//$options['after']['deploy-symlink'][] = 'deploy_cache_task';
 $options['after']['deploy-symlink'][] = 'deploy_cache_task2';
 
 /**
  * The task needs to be defined with a @task "decorator" in the comment block preceding it
  * @task
  */
-function deploy_cache_task($d) {
-    $d->run_once("cd ~/var/www/vhosts/drushdep.uat.link/httpdocs/ && %s cc all -y", $d->sites[0]['path-aliases']['%drush-script']);
-}
+//function deploy_cache_task($d) {
+//    $d->run_once("cd ~/var/www/vhosts/drushdep.uat.link/httpdocs/ && %s cc all -y", $d->sites[0]['path-aliases']['%drush-script']);
+//}
 function deploy_cache_task2($d) {
     $d->run_once("cd ~/var/www/vhosts/drushdep.uat.link/httpdocs/ && %s en module_filter -y", $d->sites[0]['path-aliases']['%drush-script']);
 }
