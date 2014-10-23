7da70ddc6bd981bdcb1e0bf9e5663ae8dbf4a9f0
Goruachev42 <mikhail.goruachev@lemberg.co.uk>
2014-10-23 13:53:03 +0300

    init

--- sites/default/deploy.drushrc.php
+++ sites/default/deploy.drushrc.php
@@ -1,6 +1,6 @@
 <?php
 $options['application'] = 'goruachev42';
-$options['deploy-repository'] = 'https://github.com/Goruachev42/KievCamp14.git';
+$options['deploy-repository'] = 'git@github.com:Goruachev42/somt.git';
 $options['branch'] = "master";
 $options['keep-releases'] = 3;
 $options['deploy-via'] = 'RemoteCache';
@@ -10,7 +10,7 @@ $options['deploy-via'] = 'RemoteCache';
 $options['git_enable_submodules'] = TRUE;
 // Run additional tasks after the 'current' symlink has been updated
 
-$options['after']['deploy-symlink'][] = 'deploy_cache_task';
+$options['after']['deploy-symlink'][] = 'deploy_pull_task';
 $options['after']['deploy-symlink'][] = 'deploy_cache_task2';
 
 /**
@@ -19,11 +19,11 @@ $options['after']['deploy-symlink'][] = 'deploy_cache_task2';
  */
 
 $options['shell-aliases']['offline'] = 'variable-set -y --always-set maintenance_mode 1';
-function deploy_cache_task($d) {
+function deploy_pull_task($d) {
     $d->run_once("cd /var/www/vhosts/drushdep.uat.link/httpdocs/ && git pull origin master", $d->sites[0]['path-aliases']['%drush-script']);
 }
 function deploy_cache_task2($d) {
-    $d->run_once("cd /var/www/vhosts/drushdep.uat.link/httpdocs/ && %s en module_filter -y", $d->sites[0]['path-aliases']['%drush-script']);
+    $d->run_once("cd /var/www/vhosts/drushdep.uat.link/httpdocs/ && %s cc all -y", $d->sites[0]['path-aliases']['%drush-script']);
 }
 
 
